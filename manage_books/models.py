b27165300d9a569b350413ed3bba3216d7fe52d2
s15283 <15283@eans-nt.edu.pl>
2026-04-08 15:36:40 +0200

    Dodanie nowych modeli

--- manage_books/models.py
+++ manage_books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    COVERS = [
+        ('hardcover', 'Hardcover'),
+        ('paperback', 'Paperback'),
+        ('ebook', 'E-book'),
+        ('audiobook', 'Audiobook'),
+    ]
+
+    LANGUAGES = [
+        ('english', 'English'),
+        ('polish', 'Polish'),
+        ('hebrew', 'Hebrew'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, unique=True)
+    publication_date = models.DateField()
+    pages = models.IntegerField()
+    cover = models.CharField(max_length=20)
+    language = models.CharField(max_length=20)
+    is_read = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+class Author(models.Model):
+
+class Publisher(models.Model):
+
+class Gener(models.Model):
+
+class Series(models.Model):
+
+class Topic(models.Model):
+
+class Category(models.Model):
+
+class Note(models.Model):
