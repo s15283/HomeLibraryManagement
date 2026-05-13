6c46dab2747a104e7f680599c7505e794d655839
s15283 <15283@eans-nt.edu.pl>
2026-05-13 16:16:28 +0200

    Projekt struktury html

--- manage_books/models.py
+++ manage_books/models.py
@@ -3,16 +3,20 @@ import pytz
 
 # Create your models here.
 class Book(models.Model):
+
     COVERS = [
         ('hardcover', 'Hardcover'),
         ('paperback', 'Paperback'),
         ('ebook', 'E-book'),
         ('audiobook', 'Audiobook'),
     ]
+
     LANGUAGES = [
         ('english', 'English'),
-        ('polish', 'Polish'),
-        ('hebrew', 'Hebrew'),
+        ("polish", 'Polish'),
+        ('spanish', 'Spanish'),
+        ('french', 'French'),
+        ('german', 'German'),
         ('other', 'Other'),
     ]
 
@@ -29,20 +33,20 @@ class Book(models.Model):
     series = models.ForeignKey('Series', on_delete=models.RESTRICT, blank=True, null=True)
     genres = models.ManyToManyField('Genre', related_name='books', blank=True)
     topics = models.ManyToManyField('Topic', related_name='books', blank=True)
-    
+
 class Author(models.Model):
     TITLES = [
-        ('ks', 'Ks.'),
-        ('dr', 'Dr.'),
-        ('prof', 'Prof.'),
-        ('bp', 'Bp.'),
-    ]
-
+            ('ks', "Ks."),
+            ('dr', "Dr."),
+            ('prof', "Prof."),
+            ('mr', "Mr."),
+            ('ms', "Ms."),
+        ]
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    alias = models.CharField(max_length=100, blank=True, null=True)
     nationality = models.CharField(max_length=100)
-    title = models.CharField(max_length=50, choices=TITLES, blank=True, null=True)
+    title = models.CharField(max_length=20, choices=TITLES, blank=True, null=True)
+    alias = models.CharField(max_length=100, blank=True, null=True)
 
 class Publisher(models.Model):
     name = models.CharField(max_length=200)
@@ -50,20 +54,20 @@ class Publisher(models.Model):
     founded_year = models.IntegerField()
     website = models.URLField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
+    # books = models.ManyToOneField(Book, related_name='publisher')
 
-class Genre(models.Model): #gatunek (literacki)
+class Genre(models.Model):
     name = models.CharField(max_length=100)
 
-class Series(models.Model): #seria (książek)
+class Series(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    authors = models.ManyToManyField(Author, related_name='series', blank=True)
 
-class Topic(models.Model): #tematyka (książki)
+class Topic(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
 class Note(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,)
--- manage_books/views.py
+++ manage_books/views.py
@@ -1,4 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import HttpResponse, render
+from manage_books.models import Genre, Author
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    genres = Genre.objects.all()
+    authors = Author.objects.all()
+    return render(request, 'manage_books/index.html', {'genres': genres, 'authors': authors})
+
+def book(request, book_id):
+    return render(request, 'manage_books/book.html', {'book_id': book_id})
+
+def author(request, author_id):
+    return render(request, 'manage_books/author.html', {'author_id': author_id})
+
+def publisher(request, publisher_id):
+    return render(request, 'manage_books/publisher.html', {'publisher_id': publisher_id})
+
+def series(request, series_id):
+    return render(request, 'manage_books/series.html', {'series_id': series_id})
+
+def note(request, note_id):
+    return render(request, 'manage_books/note.html', {'note_id': note_id})
